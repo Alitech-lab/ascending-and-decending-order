10f7765bda775b9ff92e8082c846937eba93cbbd
ALITECH <109954579+Alitech-lab@users.noreply.github.com>
2022-08-19 18:50:21 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ascending_and_decending_order
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("aseccending order");
+
+            // declaring and initialing the array 
+
+            int[] arr = { 3, 56, 5, 6, 99, 45, 10, 7, 1, 63, 12 };
+
+            // sort array in aseccending order .
+            Array.Sort(arr);
+            foreach (int i in arr)
+                Console.WriteLine(i);
+            Console.WriteLine("deccending order");
+
+
+            // reverse array
+            Array.Reverse(arr);
+            foreach (int value in arr)
+            {
+
+                Console.WriteLine(value + " ");
+
+            }
+
+        }
+
+    }
+
+
+}
+
+
